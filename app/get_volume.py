49493b5e322cf1c767189799eb73b40d890120c8
Light.Zhang <zhilight@gmail.com>
2016-05-06 00:24:08 +0800

    完成抓取所有股票历史行情的功能

--- app/get_volume.py
+++ app/get_volume.py
@@ -14,19 +14,16 @@ db_config = load(data_stream, Loader=Loader)
 # connect_url = 'postgres://%s:%s@%s/%s' % (USER, PASSWORD, HOST, DATABASE)
 engine = create_engine('postgres://light:1204516@localhost/stock_20160426')
 
-# 一次获取全部日 K 线数据
-t = ts.get_hist_data('600848')
-
-# 获取沪深上市公司的基本情况
-ts.get_stock_basics()
 
 conn = psycopg2.connect(host=db_config['production']['host'], database=db_config['production']['database'], user=db_config['production']['username'], password=db_config['production']['password'])
 
 cur = conn.cursor()
 
-cur.execute('select code from stock_basics;')
+cur.execute('select distinct code from stock_basics;')
 
 r = cur.fetchall()
 
 for row in r:
-    print(row[0])
+    print('获取 ' + row[0] + '的历史行情')
+    d = ts.get_hist_data(row[0])
+    d.to_sql('hist_data', engine, if_exists='append')
