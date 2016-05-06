e316750a4464210ccec08700a9b40298f32d4b12
Light.Zhang <zhilight@gmail.com>
2016-05-06 22:09:22 +0800

    删除不必要的文件

--- app/ana.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# encoding: utf-8
-
-import numpy as np
-import pandas
-
-
-def ana_data():
-    pass
--- app/get_data.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- encoding: utf-8 -*-
-
-import tushare as ts
-
-ts.get_stock_basics()
-ts.get_report_data(2014, 3)
-
