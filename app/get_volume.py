b1ef47cc0f9a81e4940fa183c822aac0a71e19a4
Light.Zhang <zhilight@gmail.com>
2016-05-09 23:41:13 +0800

    修改项目配置文件

--- app/get_volume.py
+++ app/get_volume.py
@@ -1,6 +1,7 @@
 import tushare as ts
 import psycopg2
 from sqlalchemy import create_engine
+from sqlalchemy.dialects.postgresql import *
 
 from yaml import load
 
@@ -11,9 +12,15 @@ except:
 data_stream = open('../config/database.yml', 'r')
 db_config = load(data_stream, Loader=Loader)
 
+USER = db_config['production']['username']
+HOST = db_config['production']['host']
+PASSWORD = db_config['production']['password']
+DATABASE = db_config['production']['database']
 
-# connect_url = 'postgres://%s:%s@%s/%s' % (USER, PASSWORD, HOST, DATABASE)
-engine = create_engine('postgres://light:1204516@localhost/stock_20160426')
+
+
+
+engine = 'postgres://%s:%s@%s/%s' % (USER, PASSWORD, HOST, DATABASE)
 
 
 conn = psycopg2.connect(host=db_config['production']['host'], database=db_config['production']['database'], user=db_config['production']['username'], password=db_config['production']['password'])
@@ -26,6 +33,6 @@ r = cur.fetchall()
 
 for row in r:
     print('获取 ' + row[0] + '的历史行情')
-    d = ts.get_hist_data(row[0])
-    d['code'] = row[0]
-    d.to_sql('hist_data', engine, if_exists='append')
+#    d = ts.get_hist_data(row[0])
+#    d['code'] = row[0]
+#    d.to_sql('hist_data', engine, if_exists='append')
