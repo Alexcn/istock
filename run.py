403d73d1d7dd4ca724f0563506e123a196bca43a
Light.Zhang <zhilight@gmail.com>
2016-06-23 01:05:50 +0800

    初始化命令行选项

--- run.py
+++ run.py
@@ -5,7 +5,30 @@
 import argparse
 
 
-parser = argparse.ArgumentParse(description='Command line tool to get data.')
+parser = argparse.ArgumentParser(description='Command line tool to manage the project.')
 
-def main():
-    pass
+
+parser.add_argument(dest='filenames',metavar='filename', nargs='*')
+
+parser.add_argument('-p', '--pat',metavar='pattern', required=True,
+                    dest='patterns', action='append',
+                    help='text pattern to search for')
+
+parser.add_argument('-v', dest='verbose', action='store_true',
+                    help='verbose mode')
+
+parser.add_argument('-o', dest='outfile', action='store',
+                    help='output file')
+
+parser.add_argument('--speed', dest='speed', action='store',
+                    choices={'slow','fast'}, default='slow',
+                    help='search speed')
+
+args = parser.parse_args()
+
+# Output the collected arguments
+print(args.filenames)
+print(args.patterns)
+print(args.verbose)
+print(args.outfile)
+print(args.speed)
