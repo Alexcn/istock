13f2609da23c9fd902617ef7ac7bf1616ed90d9f
张志亮 <zhilight@gmail.com>
2016-11-04 00:34:01 +0800

    风格调整

--- run.py
+++ run.py
@@ -8,9 +8,9 @@ import argparse
 parser = argparse.ArgumentParser(description='Command line tool to manage the project.')
 
 
-parser.add_argument(dest='filenames',metavar='filename', nargs='*')
+parser.add_argument(dest='filenames', metavar='filename', nargs='*')
 
-parser.add_argument('-p', '--pat',metavar='pattern', required=True,
+parser.add_argument('-p', '--pat', metavar='pattern', required=True,
                     dest='patterns', action='append',
                     help='text pattern to search for')
 
@@ -21,7 +21,7 @@ parser.add_argument('-o', dest='outfile', action='store',
                     help='output file')
 
 parser.add_argument('--speed', dest='speed', action='store',
-                    choices={'slow','fast'}, default='slow',
+                    choices={'slow', 'fast'}, default='slow',
                     help='search speed')
 
 args = parser.parse_args()
